e0262a5c36c098b530ec7eff5413d855d343de66
Aki Karkkainen <etimiti@gmail.com>
2015-10-09 20:19:30 +0200

    Add simple demo component

---
+++ app/cardcomponent.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import './cardcomponent.css!';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class CardComponent extends React.Component {
+  render() {
+    return (
+      <div className="card-component">
+        <h3 className="card-component__title">{this.props.title}</h3>
+        <div className="card-component__content">
+          <p>I am a very simple card.</p>
+        </div>
+      </div>
+    );
+  }
+}
